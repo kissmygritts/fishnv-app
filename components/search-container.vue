6153d0fb5a253fe4b11811c6191aa2567c1b56ab
kissmygritts <kissmygritts08@gmail.com>
2020-09-29 11:22:05 -0700

    Feature: added county and region filters
    Still waiting on water type classification, however, I've prepped
    the UI. All that needs to be done is to add:
    
    @change="selected" to the water type element.

--- components/search-container.vue
+++ components/search-container.vue
@@ -56,23 +56,139 @@
           class="block appearance-none w-full bg-white px-4 py-3 pr-8 rounded-md shadow-sm leading-tight focus:outline-none focus:shadow-outline"
         >
           <option value="" />
-          <option value="creek">
-            creek
-          </option>
-          <option value="stream">
-            stream
+          <option value="creek or stream">
+            Creek or Stream
           </option>
           <option value="river">
-            river
+            River
           </option>
-          <option value="pond">
-            pond
+          <option value="lake or reservoir">
+            Lake or Reservoir
           </option>
-          <option value="lake">
-            lake
+          <option value="urban fishery">
+            Urban Fishery
           </option>
-          <option value="reservoir">
-            reservoir
+        </select>
+        <div
+          class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+        >
+          <svg
+            class="w-4 h-4"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path d="M19 9l-7 7-7-7" />
+          </svg>
+        </div>
+      </div>
+    </div>
+
+    <!-- select by region -->
+    <div class="py-2">
+      <label for="water-type">Select NDOW Region</label>
+      <div class="relative inline-block w-full mt-1">
+        <select
+          id="region"
+          v-model="region.selected"
+          name="region"
+          class="block appearance-none w-full bg-white px-4 py-3 pr-8 rounded-md shadow-sm leading-tight focus:outline-none focus:shadow-outline"
+          @change="selected"
+        >
+          <option value="" />
+          <option value="Eastern">
+            Eastern
+          </option>
+          <option value="Southern">
+            Southern
+          </option>
+          <option value="Western">
+            Western
+          </option>
+        </select>
+        <div
+          class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+        >
+          <svg
+            class="w-4 h-4"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path d="M19 9l-7 7-7-7" />
+          </svg>
+        </div>
+      </div>
+    </div>
+
+    <!-- select by county -->
+    <div class="py-2">
+      <label for="water-type">Select County</label>
+      <div class="relative inline-block w-full mt-1">
+        <select
+          id="county"
+          v-model="county.selected"
+          name="county"
+          class="block appearance-none w-full bg-white px-4 py-3 pr-8 rounded-md shadow-sm leading-tight focus:outline-none focus:shadow-outline"
+          @change="selected"
+        >
+          <option value="" />
+          <option value="Carson City">
+            Carson City
+          </option>
+          <option value="Churchill">
+            Churchill
+          </option>
+          <option value="Clark">
+            Clark
+          </option>
+          <option value="Douglas">
+            Douglas
+          </option>
+          <option value="Elko">
+            Elko
+          </option>
+          <option value="Esmeralda">
+            Esmeralda
+          </option>
+          <option value="Eureka">
+            Eureka
+          </option>
+          <option value="Humboldt">
+            Humboldt
+          </option>
+          <option value="Lander">
+            Lander
+          </option>
+          <option value="Lincoln">
+            Lincoln
+          </option>
+          <option value="Lyon">
+            Lyon
+          </option>
+          <option value="Mineral">
+            Mineral
+          </option>
+          <option value="Nye">
+            Nye
+          </option>
+          <option value="Pershing">
+            Pershing
+          </option>
+          <option value="Storey">
+            Storey
+          </option>
+          <option value="Washoe">
+            Washoe
+          </option>
+          <option value="White Pine">
+            White Pine
           </option>
         </select>
         <div
@@ -104,6 +220,12 @@ export default {
   data () {
     return {
       waterType: {
+        selected: null
+      },
+      region: {
+        selected: null
+      },
+      county: {
         selected: null
       },
       species: {
@@ -153,7 +275,9 @@ export default {
     searchFilters () {
       const filters = {
         species: this.species.selected,
-        water_type: this.waterType.selected
+        water_type: this.waterType.selected,
+        region: this.region.selected,
+        county: this.county.selected
       }
 
       return removeNullish(filters)
@@ -170,6 +294,8 @@ export default {
     clearFilters () {
       this.waterType.selected = null
       this.species.selected = null
+      this.region.selected = null
+      this.county.selected = null
 
       // call selected method to run search
       this.selected()
